f3a685f637528ffbb0c54f861bc69bb57558a19e
Nikhil <nikhil.jmsyadav@gmail.com>
2024-12-05 17:04:38 +0530

    BMI calculator project

---
+++ BMI.js
@@ -0,0 +1,41 @@
+window.onload= ()=>{
+    let button =document.querySelector('#btn');
+    button.addEventListener('click', calculateBMI);
+}
+function calculateBMI(){
+    let height = document.querySelector("#height").value;
+    let weight = document.querySelector(" #weight").value;
+    let result = document.querySelector("#result");
+    // console.log(document.querySelector("#height").value);
+    // console.log(document.querySelector("#weight").value);
+
+    if(!height || isNaN(height) || height<0 ){
+        result.innerText="Please enter valid height";
+        return;
+    }
+    else if(!weight || isNaN(weight) || weight<0 ){
+        result.innerText="Please enter valid weight";
+        return;
+    }
+
+    let BMI = (weight/((height*height)/10000)).toFixed(2);
+
+    if (BMI<18.5){
+        result.innerText=`Under weight: ${BMI}`;
+    }
+    else if(BMI>=18.5 && BMI<24.9){
+        result.innerText=`Normal: ${BMI}`;
+    }
+    else if(BMI>=18.5 && BMI<24.9){
+        result.innerText=`Over weight: ${BMI}`;
+    }
+    else if(BMI>=18.5 && BMI<24.9){
+        result.innerText=`Obesity I: ${BMI}`;
+    }
+    else if(BMI>=18.5 && BMI<24.9){
+        result.innerText=`Obesity II: ${BMI}`;
+    }
+    else{
+        result.innerText=`Extreme Obesity: ${BMI}`;
+    }
+}
